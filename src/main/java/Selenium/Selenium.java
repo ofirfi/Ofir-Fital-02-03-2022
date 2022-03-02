bbc0e06afc45c384d5068824a9c4c2b6fe65d869
ofirfi <OfirFital@gmail.com>
2022-03-02 23:03:38 +0200

    Added basic selenium functions

--- src/main/java/Selenium/Selenium.java
+++ src/main/java/Selenium/Selenium.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class Selenium {
@@ -26,5 +27,34 @@ public class Selenium {
         return selenium;
     }
 
+    public void goToURL(String url){
+        driver.get(url);
+    }
+
+    public String getURL(){
+        return driver.getCurrentUrl();
+    }
+
+    public void clickOnElement(WebElement element){
+        element.click();
+    }
+
+    public void verifyElementText(WebElement element,String text){
+        if(!getElementText(element).equals(text))
+            System.out.println("Exepction or warning");
+    }
+
+    public void verifyElementsTexts(List<WebElement> elements, List<String> texts){
+        for (int i=0;i<elements.size();i++)
+            verifyElementText(elements.get(i),texts.get(i));
+    }
+
+    public String getElementText(WebElement element){
+        return element.getText();
+    }
+
+    public void close(){
+        driver.close();
+    }
 
 }
