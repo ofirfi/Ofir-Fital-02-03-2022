4fe06fcb327fa3314ba8bedf3c74ddcfdb8e3226
ofirfi <OfirFital@gmail.com>
2022-03-03 19:19:04 +0200

    Added shared elements

--- src/main/java/Shared/BaseObjects.java
+++ src/main/java/Shared/BaseObjects.java
@@ -1,6 +1,8 @@
 package Shared;
 
 import Selenium.Selenium;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BaseObjects {
@@ -9,4 +11,11 @@ public class BaseObjects {
     public BaseObjects(){
         PageFactory.initElements(selenium.getDriver(),this);
     }
+
+    @FindBy(xpath = "//a[contains(@href,'facebook.com')]")
+    protected WebElement facebookLink;
+
+    @FindBy(xpath = "//a[contains(@href,'linkedin.com')]")
+    protected WebElement linkedinLink;
+
 }
